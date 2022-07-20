ce3c2615c4f503d3552afcb100a94a7ab4da3251
wink68 <108077414+wink68@users.noreply.github.com>
2022-07-20 17:59:19 +0900

    Delete test02.py

--- test02.py
+++
@@ -1,7 +0,0 @@
-def solution(a, b):
-    print('덧셈 값: ', int(a)+int(b))
-    print('뺄셈 값: ', int(a)-int(b))
-    print('곱셈 값: ', int(a)*int(b))
-    print('나눗셈 값: ', int(a)/int(b))
-
-solution(1, 2)
